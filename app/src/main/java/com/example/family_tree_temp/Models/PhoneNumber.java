2a5906fabdac94a2cf2d2333a0585fb8e3d208ce
acomanzo <ansonih17@gmail.com>
2021-03-24 23:23:36 -0400

    update phone number so that foreign key points to contact information

--- app/src/main/java/com/example/family_tree_temp/Models/PhoneNumber.java
+++ app/src/main/java/com/example/family_tree_temp/Models/PhoneNumber.java
@@ -6,9 +6,9 @@ import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
 @Entity(foreignKeys = {@ForeignKey(
-        entity = FamilyMember.class,
-        parentColumns = "familyMemberId",
-        childColumns = "familyMemberId",
+        entity = ContactInformation.class,
+        parentColumns = "contactInformationId",
+        childColumns = "contactInformationId",
         onDelete = ForeignKey.CASCADE
 )})
 public class PhoneNumber {
@@ -18,14 +18,14 @@ public class PhoneNumber {
     private int phoneNumberId;
 
     @NonNull
-    private int familyMemberId;
+    private int contactInformationId;
 
     @NonNull
     private String phoneNumber;
 
-    public PhoneNumber(@NonNull int phoneNumberId, @NonNull int familyMemberId, @NonNull String phoneNumber) {
+    public PhoneNumber(@NonNull int phoneNumberId, @NonNull int contactInformationId, @NonNull String phoneNumber) {
         this.phoneNumberId = phoneNumberId;
-        this.familyMemberId = familyMemberId;
+        this.contactInformationId = contactInformationId;
         this.phoneNumber = phoneNumber;
     }
 
@@ -33,8 +33,8 @@ public class PhoneNumber {
         return phoneNumberId;
     }
 
-    public int getFamilyMemberId() {
-        return familyMemberId;
+    public int getContactInformationId() {
+        return contactInformationId;
     }
 
     public String getPhoneNumber() {
