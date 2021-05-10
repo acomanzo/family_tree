f0553f2c7a9d3b9b692c398b2272a38ad8fd1ea4
acomanzo <ansonih17@gmail.com>
2021-05-10 15:22:37 -0400

    moved content of MainActivity into TreeEditorActivity

--- app/src/main/java/com/example/family_tree_temp/Activities/WelcomeActivity.java
+++ app/src/main/java/com/example/family_tree_temp/Activities/WelcomeActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.family_tree_temp.R;
+
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
